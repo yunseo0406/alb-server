65422dfe34c1808e44f37aa1631f20315e905908
yunseo0406 <lcss0406@gmail.com>
2025-08-13 16:12:53 +0900

    key_name 출력 수정

--- keypair.tf
+++ keypair.tf
@@ -3,6 +3,11 @@ resource "ncloud_login_key" "this" {
   key_name = "login-key-${formatdate("YYYYMMDDhhmmss", timestamp())}"
 }
 
+# login_key_name 출력
+output "login_key_name" {
+  value = ncloud_login_key.this.key_name
+}
+
 # private_key 출력 (민감정보라 sensitive 설정)
 output "private_key" {
   value     = ncloud_login_key.this.private_key
